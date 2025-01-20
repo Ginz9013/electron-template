6dd5471c02b9c96322fa37e7c6abfa4e4b058819
Alan Ho <alan.ho@contin-global.com>
2025-01-20 11:31:55 +0800

    feat: 新增 shadcn form, 修改 login 頁面表單邏輯

--- src/renderer/pages/login/Login.tsx
+++ src/renderer/pages/login/Login.tsx
@@ -1,40 +1,93 @@
 import { useNavigate } from 'react-router-dom';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { Button } from '@/src/renderer/components/ui/button';
 import { Input } from '@/src/renderer/components/ui/input';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/src/renderer/components/ui/form';
+import { useUser } from '@/src/renderer/hooks/useUser';
 import CenterLayout from '@/src/renderer/components/layout/Center';
 import icon from '@/assets/icon.svg';
+
+const formSchema = z.object({
+  account: z.string().min(2).max(50),
+  password: z.string().min(2).max(50),
+});
 
 const { CenterContainer } = CenterLayout;
 
 const Login = () => {
+  const [ _, setUser ] = useUser();
   const navigate = useNavigate();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      account: '',
+      password: '',
+    },
+  });
+
+  const submitHandler = (values: z.infer<typeof formSchema>) => {
+    setUser({ token: values.account + values.password });
+    navigate('/dashboard');
+  };
 
   return (
     <CenterLayout>
       <CenterContainer>
         <img width="160" alt="icon" src={icon} />
         <h1 className="text-white text-2xl mb-4">electron-react-boilerplate</h1>
-        {/* Account */}
-        <div className="w-full">
-          <label htmlFor="account" className="text-white text-sm">
-            Account
-          </label>
-          <Input id="account" className="text-white" />
-        </div>
-        {/* Password */}
-        <div className="w-full">
-          <label htmlFor="password" className="text-white text-sm">
-            Password
-          </label>
-          <Input id="password" className="text-white" />
-        </div>
-        <Button
-          className="bg-white text-slate-900 hover:bg-slate-300 w-full font-bold mt-4"
-          // eslint-disable-next-line no-console
-          onClick={() => navigate('/dashboard')}
-        >
-          Login
-        </Button>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(submitHandler)}
+            className="space-y-4 w-full flex flex-col"
+          >
+            <FormField
+              control={form.control}
+              name="account"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-white">Account</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Please enter your account..."
+                      className="text-white"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-white">Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Please enter your password..."
+                      className="text-white"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </CenterContainer>
     </CenterLayout>
   );
